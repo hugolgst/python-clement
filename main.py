1f669792d2c7236ed470305a948e919f95cb3a0b
Hugo Lageneste <hugo.lageneste@pm.me>
2024-11-07 14:43:33 +0100

    Read all CSVs

--- main.py
+++ main.py
@@ -2,4 +2,21 @@ import pandas as pd
 import json
 
 CCI = pd.read_csv('data/CCI.csv')
-print(CCI)
+CCI['DATE'] = pd.to_datetime(CCI['DATE'])
+
+CPI = pd.read_csv('data/CPI.csv')
+CPI['DATE'] = pd.to_datetime(CPI['DATE'])
+
+Housing = pd.read_csv('data/Housing.csv')
+Housing['DATE'] = pd.to_datetime(Housing['DATE'])
+
+IPI = pd.read_csv('data/IndustrialPriceIndex.csv')
+IPI['DATE'] = pd.to_datetime(IPI['DATE'])
+
+PMI = pd.read_csv('data/PMI.csv')
+PMI['DATE'] = pd.to_datetime(PMI['DATE'])
+
+PPI = pd.read_csv('data/PPI.csv')
+PPI['DATE'] = pd.to_datetime(PPI['DATE'])
+
+print(PMI)
